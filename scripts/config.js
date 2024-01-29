09f2041f25bc985c5f1756e7d21cc112aa9e08a0
Charlotte Millet <charlottemillet04@gmail.com>
2024-01-30 00:27:17 +0100

    added dropdown

---
+++ scripts/config.js
@@ -0,0 +1,62 @@
+const RECIPES = {
+    "Salade César":{
+        "ID": 1,
+        "Nombre de personnes": 2,
+        "Tags": ["Salade"],
+        "Ingrédients": {
+            "batavia": 40,
+            "poulet": 200
+        }
+    },
+
+    "Bowl Poulet":{
+        "ID": 2,
+        "Nombre de personnes": 2,
+        "Tags": [],
+        "Ingrédients": {
+            "avocat": 1,
+            "mais": 100,
+            "filet de poulet": 2
+        }
+    },
+
+    "Cuisse de poulet et légumes rôtis au thym":{
+        "ID": 3,
+        "Nombre de personnes": 2,
+        "Tags": [],
+        "Ingrédients": {
+            "poivron rouge": 1,
+            "courgette": 1,
+            "tomates cerises": 250,
+            "cuisse de poulet": 2,
+            "thym séche": 2,
+            "farfalle": 140,
+            "copeaux de grana padano": 40,
+            "huile d'olive": 1.5,
+            "beurre": 1
+        }
+    }
+}
+
+const INGREDIENTS = {
+    "batavia": ["g de ", "légume", "batavia", "batavia"],
+    "poulet": ["g de ", "viande", "poulet", "poulet"],
+    "filet de poulet": ["", "viande", "filet de poulet", "filets de poulet"],
+    "avocat": ["", "légume", "avocat", "avocats"],
+    "mais": ["g de ", "légume", "maïs", "maïs"],
+    "haricots rouges": ["g d'", "légume", "haricots rouges", "haricots rouges"],
+    "riz": ["g de ", "féculent", "riz", "riz"],
+    "galette d'épeautre": ["", "galette", "galette d'épeautre", "galettes d'épeautre"],
+    "poireau(x)": ["", "légume", "poireau", "poireaux"],
+    "spaghetti": ["g de ", "féculent", "spaghetti", "spaghetti"],
+    "lardons": ["g de ", "viande", "lardons", "lardons"],
+    "poivron rouge": ["", "légume", "poivron rouge", "poivrons rouges"],
+    "courgette": ["", "légume", "courgette", "courgettes"],
+    "tomates cerises": ["g de ", "légume", "tomates cerises", "tomates cerises"],
+    "cuisse de poulet": ["", "viande", "cuisse de poulet", "cuisses de poulet"],
+    "thym séché": ["cc de ", "condiment", "thym séché", "thym séché"],
+    "farfalle": ["g de ", "féculent", "farfalle", "farfalle"],
+    "copeaux de grana padano": ["g de ", "fromage", "copeaux de grana padano", "copeaux de grana padano"],
+    "huile d'olive": ["cs d'", "condiment", "huile d'olive", "huile d'olive"],
+    "beurre": ["g de ", "condiment", "beurre", "beurre"]
+}
--- scripts/script.js
+++ scripts/script.js
@@ -4,20 +4,29 @@ function addRecipe(id){
 
     let new_recipes = document.createElement("div")
     new_recipes.id = `recipe_${id}`
+
+    let multiselect_recipes = ""
+    // console.log(_.get(RECIPES, recipe, undefined))
+    Object.keys(RECIPES).forEach( (recipe, i) =>{
+        let recette = RECIPES[recipe]
+        multiselect_recipes = `${multiselect_recipes}
+        <option value="${RECIPES[recipe]["ID"]}">${Object.keys(RECIPES)[i]}</option>`       
+    })
+
     let div_content = `
     <span id="minus_${id}" class="minus">-</span>
     1
     <span id="plus_${id}" class="plus">+</span>
-    <div class="multiselector">Je choisirai ma recette plus tard</div>
     <label for="recipe_name_${id}">Quelle recette ?</label><br>
         <select name="recipe_name_${id}" id="name_recipe_${id}">
-            <option value="espagne">Espagne</option>
-            <option value="royaume-uni">Royaume-Uni</option>
-            <option value="canada">Canada</option>
-            <option value="japon">Japon</option>
+            ${multiselect_recipes}
         </select>
     <span id="cross_${id}" class="delete_recipe">x</span>
     `
+    // <option value="espagne">Espagne</option>
+    // <option value="royaume-uni">Royaume-Uni</option>
+    // <option value="canada">Canada</option>
+    // <option value="japon">Japon</option>
     new_recipes.innerHTML = div_content
     myDiv.appendChild(new_recipes);
 
@@ -25,7 +34,6 @@ function addRecipe(id){
     let div_id_to_remove = `recipe_${id}`
     btnRemoveRecipe.addEventListener("click", () => {
         RemoveRecipe(div_id_to_remove)
-        // document.getElementById().remove()
     })
     id = id + 1
 
@@ -38,12 +46,25 @@ function RemoveRecipe(id_div){
     DivRecipe.remove();
 }
 
+function getSelectedRecipes(id){
+    let Div = document.querySelectorAll(`#recipe_2 select`)[0]
+    console.log(Div)
+    console.log(Div.value)
+    // let aa = Div.options[Div.selectedIndex].text
+    return Div.value
+}
+
 function main() {
     id = 0
 
     let btnAddRecipe = document.getElementById("add_recettes")
-    
     btnAddRecipe.addEventListener("click", () => {
         id = addRecipe(id)
+    })
+
+    let btnValidate = document.getElementById("validate_button")
+    btnValidate.addEventListener("click", () => {
+        let selected_recipes = getSelectedRecipes(id)
+        console.log(selected_recipes)
     })
 }
